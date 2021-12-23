8c1d31a0a6aa513d977fd6ede6c73b2e1007e82a
atmygml <43545518+atmygml@users.noreply.github.com>
2021-12-23 23:35:59 +0800

    Create container.dart

---
+++ container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {   
+    return Center(
+      child: Container(        
+        padding: const EdgeInsets.all(16),     
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),      
+        decoration: const BoxDecoration(         
+          image: DecorationImage(          
+            image: AssetImage('assets/mag1.png'),            
+            fit: BoxFit.cover,
+          ),         
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+      ),
+    );
+  }
+}
